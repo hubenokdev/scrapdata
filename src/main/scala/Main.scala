e40091a7ca0a4f68f179bd00b6c3c92638682dd9
Senior Backend Engineer <114651975+hubenokdev@users.noreply.github.com>
2022-12-02 05:57:26 +0900

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,7 +10,7 @@ object Main {
       val contents = get_contents_from_url(args(0))
 
       contents.foreach(println)
-      println("----------------------")   
+      println("---------------------")   
     }
 
     def get_contents_from_url(url: String): List[String] = {
@@ -19,16 +19,48 @@ object Main {
 
       // find title tag
       val hTitle = doc >> element(".buysell-title") >> element("h1")
-      // find requirement content tag
-      val pElements = doc >> element(".buysell-main-container") >> element(".buysell-details-column")
-      
-      // split with break string
-      val arrays_htags = pElements.innerHtml.split("\\n")
 
-      // remove html tags <br>, <b>, </bget contents data 
-      val retval = List("Title: " + hTitle.text) ++ arrays_htags.map(_.replace("<br>", "").replace("<b>", "").replace("</b>", ""))
+      var retval =  List("Title: " + hTitle.text)
+
+      // get state name
+      val pMainContainer = doc >> element(".buysell-main-container") 
+      for (pelement <- pMainContainer.children){ 
+        if (pelement.outerHtml contains "buysell-container-right buysell-profileinfo")
+        {
+          //println(pelement.outerHtml)
+          val idx = pelement.outerHtml indexOf "gif\" alt="
+          if (idx > 0)
+          {
+            var s_region = pelement.outerHtml.substring(idx + 12)
+            val idx_end = s_region indexOf "</span>"
+            retval :+= "region: " + s_region.substring(0, idx_end)
+          }
+        }
+      }
       
- 
+      // get price value
+      retval :+= ("Price : " + (doc >> element(".buysell-main-container") >> element(".buysell-container-right")).text)
+
+      // get specs of good
+      val pElements = doc >> element(".buysell-main-container") >> elements(".buysell-details-column")
+      for (pElement <- pElements){  
+        // split with break string
+        val inner_tags = pElement.innerHtml.split("\\n").map(_.replace("<br>", "").replace("<b>", "").replace("</b>", "")) 
+        for (item <- inner_tags)
+        {
+          retval :+= item
+        }
+      }
+
+      // get description text
+      val pContainerLeft = doc >> element(".buysell-container-left")
+      for (pelement <- pContainerLeft.children){ 
+        if (pelement.outerHtml contains "buysell-container description")
+          {
+            retval :+= ("Description: " + pelement.text)
+          }
+      } 
+
       retval  
     }
 }
